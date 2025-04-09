a808ce45f1b98bb6660feaa982d8085a090d75a1
tg112 <info.tgw112@gmail.com>
2025-04-09 14:05:53 +0900

    cdk basic

--- cdk_practice/cdk_practice_stack.py
+++ cdk_practice/cdk_practice_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    CfnOutput,
+    Fn
 )
 from constructs import Construct
 
@@ -10,10 +12,24 @@ class CdkPracticeStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        suffix = self.__initialize_suffix()
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkPracticeQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        self.bucket = s3.Bucket(self, "PyBucket",
+            bucket_name=f"bucket-{suffix}",
+            lifecycle_rules=[
+                s3.LifecycleRule(
+                    expiration=Duration.days(3),
+                ),
+            ]
+        )
+        print('Bucket name: ' + self.bucket.bucket_name)
+        CfnOutput(self, 'BucketName', value=self.bucket.bucket_name)
+
+    def __initialize_suffix(self):
+        short_stack_id = Fn.select(2, Fn.split('/', self.stack_id))
+        suffix = Fn.select(4, Fn.split('-', short_stack_id))
+        return suffix
+
+    @property
+    def my_bucket(self):
+        return self.bucket
