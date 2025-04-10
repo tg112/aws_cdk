ff47d688895d07425244e772cb597eb24b470e69
tg112 <info.tgw112@gmail.com>
2025-04-10 10:27:48 +0900

    cloud-formation

--- web-deploy/app.py
+++ web-deploy/app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from web_deploy.web_deploy_stack import WebDeployStack
+from web_deploy.web_deploy_stack import WebdeplStack
 
 
 app = cdk.App()
-WebDeployStack(app, "WebDeployStack",
+WebdeplStack(app, "WebDeployStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
